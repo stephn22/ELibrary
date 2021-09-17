3ace6e0a09e9830074155b60e1f9d5f6e864fa38
Stefano Crotti <51045661+stecrotti1@users.noreply.github.com>
2021-09-17 10:15:00 +0200

    Refactoring user-dao.js

--- models/user-dao.js
+++ models/user-dao.js
@@ -1,10 +1,10 @@
 "use strict";
 
-const db = require('../db.js');
-const crypt = require('bcrypt');
-const userType = require('../entities/constants/user-type');
+import { get, run } from '../db.js';
+import { compareSync, hashSync } from 'bcrypt';
+import { CUSTOMER } from '../entities/constants/user-type';
 
-const User = require('../entities/user');
+import User from '../entities/user';
 
 
 // TODO: check promises
@@ -14,11 +14,11 @@ const User = require('../entities/user');
  * @param {*} id of user.
  * @returns user
  */
-exports.getUserById = function (id) {
+export function getUserById (id) {
     return new Promise((resolve, reject) => {
         const query = "SELECT * FROM users WHERE id = ?";
 
-        db.get(query, [id], (err, row) => {
+        get(query, [id], (err, row) => {
             if (err) {
                 reject(err);
             } else if (row === undefined) {
@@ -30,7 +30,7 @@ exports.getUserById = function (id) {
             }
         });
     });
-};
+}
 
 /**
  * Get user by email and password.
@@ -38,11 +38,11 @@ exports.getUserById = function (id) {
  * @param {*} password of user.
  * @returns user
  */
-exports.getUserByEmailAndPassword = function (email, password) {
+export function getUserByEmailAndPassword (email, password) {
     return new Promise((resolve, reject) => {
         const query = "SELECT * FROM users WHERE email = ?";
 
-        db.get(query, [email], (err, row) => {
+        get(query, [email], (err, row) => {
             if (err) {
                 reject(err);
             } else if (row === undefined) {
@@ -51,32 +51,32 @@ exports.getUserByEmailAndPassword = function (email, password) {
                 // no need to add password to user object
                 const user = new User(row.id, row.fname, row.lname, row.email, undefined, row.address, row.type);
 
-                const check = crypt.compareSync(password, row.password);
+                const check = compareSync(password, row.password);
 
                 resolve({ user, check });
             }
         });
     });
-};
+}
 
 /**
  * Create user.
  * @param {*} user to be created into db.
  * @returns id of user created.
  */
-exports.createUser = function (user) {
+export function createUser (user) {
     return new Promise((resolve, reject) => {
         const query = "INSERT INTO users (fname, lname, email, password, address, type) VALUES (?, ?, ?, ?, ?, ?)";
 
-        user.password = crypt.hashSync(user.password, 10);
+        user.password = hashSync(user.password, 10);
 
-        db.run(query, [
+        run(query, [
             user.fname,
             user.lname,
             user.email,
             user.password,
             user.address,
-            userType.CUSTOMER], (err) => {
+            CUSTOMER], (err) => {
                 if (err) {
                     reject(err);
                 } else {
@@ -84,19 +84,19 @@ exports.createUser = function (user) {
                 }
             });
     });
-};
+}
 
 /**
  * Update an existing user.
  * @param {*} user to be updated.
  * @returns id of updated user.
  */
-exports.updateUser = function (user) {
+export function updateUser (user) {
     return new Promise((resolve, reject) => {
         const query = "UPDATE users SET fname = ?, lname = ?, email = ?, password = ?, address = ?, type = ? WHERE id = ?";
-        user.password = crypt.hashSync(user.password, 10);
+        user.password = hashSync(user.password, 10);
 
-        db.run(query, [
+        run(query, [
             user.fname,
             user.lname,
             user.email,
@@ -111,18 +111,18 @@ exports.updateUser = function (user) {
                 }
             });
     });
-};
+}
 
 /**
  * Delete user (by id).
  * @param {*} id id of user to be deleted.
  * @returns 
  */
-exports.deleteUser = function (id) {
+export function deleteUser (id) {
     return new Promise((resolve, reject) => {
         const query = "DELETE FROM users WHERE id = ?";
 
-        db.run(query, [id], (err) => {
+        run(query, [id], (err) => {
             if (err) {
                 reject(err);
             } else {
@@ -130,4 +130,4 @@ exports.deleteUser = function (id) {
             }
         });
     });
-};
+}
