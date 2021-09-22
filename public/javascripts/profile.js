07db13cec232ebd31fc4cb89d42e83139f9b4413
Stefano Crotti <20029190@studenti.uniupo.it>
2021-09-22 12:16:15 +0200

    Update carousel icon

--- public/javascripts/profile.js
+++ public/javascripts/profile.js
@@ -1,7 +1,8 @@
-(function () {
-    'use strict'
-    var tooltipTriggerList = [].slice.call(document.querySelectorAll('[data-bs-toggle="tooltip"]'))
-    tooltipTriggerList.forEach(function (tooltipTriggerEl) {
-      new bootstrap.Tooltip(tooltipTriggerEl)
-    })
-  })()
+"use strict";
+
+// TODO:
+/************************** CONSTANTS *****************************/
+
+
+
+/************************** EVENT LISTENERS *****************************/
