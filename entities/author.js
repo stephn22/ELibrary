03c0effe4efffde85687eff7616e8f16e3b53c37
Stefano Crotti <51045661+stecrotti1@users.noreply.github.com>
2021-09-27 10:29:11 +0200

    Add author-dao

--- entities/author.js
+++ entities/author.js
@@ -1,7 +1,17 @@
 const baseEntity = require('./baseEntity');
 
+/**
+ * @class Author represents an author of a book.
+ * @extends baseEntity extends the base class for all entities
+ */
 class Author extends baseEntity {
-    constructor (id, name) {
+
+    /**
+     * Create a new Author object
+     * @param {number} id id of the author
+     * @param {string} name name of the author
+     */
+    constructor(id, name) {
         super(id);
         this.name = name;
     }
---
+++ models/author-dao.js
@@ -0,0 +1,137 @@
+"use strict";
+
+const db = require('../db');
+const Author = require('../entities/author');
+const logger = require('../util/logger');
+
+/**
+ * Add author to database
+ * @param {Author} author author to add
+ * @returns {Promise<number>} id of added author
+ */
+function addAuthor(author) {
+    return new Promise((resolve, reject) => {
+        const query = "INSERT INTO authors (name) VALUES (?)";
+
+        db.run(query, [author.name],
+            (err) => {
+                if (err) {
+                    logger.logError(err);
+                    reject(err);
+                } else {
+                    resolve(this.lastID);
+                }
+            });
+    });
+}
+
+/**
+ * Update an author in database
+ * @param {Author} author author to update
+ * @returns {Promise<number>} id of updated author
+ */
+function updateAuthor(author) {
+    return new Promise((resolve, reject) => {
+        const query = "UPDATE authors SET name = ? WHERE id = ?";
+
+        db.run(query, [author.name, author.id],
+            (err) => {
+                if (err) {
+                    logger.logError(err);
+                    reject(err);
+                } else {
+                    resolve(author.id);
+                }
+            });
+    });
+}
+
+/**
+ * Delete an author from database
+ * @param {number} id id of author to delete
+ * @returns {Promise<number>} id of deleted author
+ */
+function deleteAuthor(id) {
+    return new Promise((resolve, reject) => {
+        const query = "DELETE FROM authors WHERE id = ?";
+
+        db.run(query, [id], (err) => {
+            if (err) {
+                logger.logError(err);
+                reject(err);
+            } else {
+                resolve(id);
+            }
+        });
+    });
+}
+
+/**
+ * Find author by id
+ * @param {number} id id of author to find
+ * @returns {Promise<Author>} author found
+ */
+function findAuthorById(id) {
+    return new Promise((resolve, reject) => {
+        const query = "SELECT * FROM authors WHERE id = ?";
+
+        db.get(query, [id], (err, row) => {
+            if (err) {
+                logger.logError(err);
+                reject(err);
+            } else if (row === undefined) {
+                logger.logWarn(`Author with id ${id} not found`);
+                resolve({ error: "No such author" });
+            } else {
+                resolve(new Author(row.id, row.name));
+            }
+        });
+    });
+}
+
+/**
+ * Return all authors that match the given name
+ * @param {string} name name of author to find
+ * @returns {Promise<Author[]>} authors found
+ */
+function findAuthorsByName(name) {
+    return new Promise((resolve, reject) => {
+        const query = "SELECT * FROM authors WHERE name LIKE ?";
+
+        db.all(query, [`%${name}%`], (err, rows) => {
+            if (err) {
+                logger.logError(err);
+                reject(err);
+            } else if (rows.length === 0) {
+                logger.logWarn(`No authors found with name ${name}`);
+                resolve({ error: "No authors found" });
+            } else {
+                resolve(rows.map(row => new Author(row.id, row.name)));
+            }
+        });
+    });
+}
+
+/**
+ * Return all authors in database as an array
+ * @returns {Promise<Author[]>} authors found
+ */
+function findAllAuthors() {
+    return new Promise((resolve, reject) => {
+        const query = "SELECT * FROM authors";
+
+        db.all(query, (err, rows) => {
+            if (err) {
+                logger.logError(err);
+                reject(err);
+            } else if (rows.length === 0) {
+                logger.logWarn("No authors found");
+                resolve({ error: "No authors found" });
+            } else {
+                resolve(rows.map(row => new Author(row.id, row.name)));
+            }
+        });
+    });
+}
+
+module.exports = { addAuthor, updateAuthor, deleteAuthor, findAuthorById, findAuthorsByName, findAllAuthors };
