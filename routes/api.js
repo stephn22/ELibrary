53494d916b533b32fba4b6bf0bdfaff21c82f001
Stefano Crotti <51045661+stecrotti1@users.noreply.github.com>
2021-09-30 16:55:13 +0200

    Update api.js

--- routes/api.js
+++ routes/api.js
@@ -7,16 +7,28 @@ const bookDao = require('../models/book-dao');
 const orderDao = require('../models/order-dao');
 const reviewDao = require('../models/review-dao');
 
+// GET all books
 router.get('/books', async (_req, res) => {
     const books = await bookDao.findAllBooks();
     res.json(books);
 });
 
+// GET book by id
+router.get('/books/:id', async function (req, res) {
+    const id = parseInt(req.params.id);
+
+    const book = await bookDao.findBookById(id);
+
+    res.json(book);
+});
+
+// GET all orders
 router.get('/orders', async (_req, res) => {
     const orders = await orderDao.findAllOrders();
     res.json(orders);
 });
 
+// GET order by id
 router.get('/reviews', async (_req, res) => {
     const reviews = await reviewDao.findAllReviews();
     res.json(reviews);
